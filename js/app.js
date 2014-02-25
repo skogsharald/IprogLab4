0495dabfaed8649ff25e891daedd0169d89d1a8a
Dexterminator <dexter@kth.se>
2014-02-25 15:44:18 +0100

    Started implementing first view.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 $(function() {
 	//The global variable so we can access it from other controller and views
-	window.stage = "starter";
-	window.alertTest = function(){
-		alert(1);
-	}
-	
+
 	//We instantiate our model
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
+	var homeView = new HomeView($("#homeView"), model);
+	var homeViewController = new HomeViewController(homeView, model);
+
 	var exampleView = new ExampleView($("#exampleView"),model);
    	var exampleViewController = new ExampleViewController(exampleView,model);
-   	// exampleView.makeHidden();
+   	exampleView.makeHidden();
+
+
+
+   	window.stage = "starter";
+	window.startApp = function(){
+		homeView.makeHidden();
+		$("body").removeClass("background-image");
+	}
+	
 });
