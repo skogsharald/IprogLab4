85d6e3542dd418220e20dde2545b1801ee48bddf
kwacktoffeln <sandragrosz@gmail.com>
2014-02-26 15:16:51 +0100

    Fixed dynamic prices and ingredients in dinnerImagesView and printPrepView

--- js/view/dinnerImagesView.js
+++ js/view/dinnerImagesView.js
@@ -53,6 +53,7 @@ var DinnerImagesView = function (container,model) {
 	container.append(secondRow);
 	
 	this.dinnerOverViewButton = container.find("#dinnerOverViewButton");
+	model.addObserver(this);
 
 	this.makeHidden = function(){
 		container.fadeOut(0, function() {
@@ -64,5 +65,61 @@ var DinnerImagesView = function (container,model) {
 		container.fadeIn(1000, function() {
 			//Animation complete
 		});
+	}
+
+	this.update = function(){
+		container.html("");
+		var dishes = model.getFullMenu();
+		var totalCost = model.getTotalMenuPrice();
+
+		// Add the first row which contains the two columns 
+		var firstRow = $("<div>").addClass("print-prep row");
+
+		// Populate the dinner images view
+		var dinnerCol = $("<div>").addClass("col-md-6 col-md-offset-3");
+		var imgRow = $("<div>").addClass("centerImgRow row");
+
+		for(i = 0; i < dishes.length; i++) {
+			var dish = dishes[i];
+			var imgCol;
+			var imgContainer;
+			var imgTag;
+			var label;
+
+			imgCol = $("<div>").addClass("col-md-4");
+			imgContainer = $("<div>").addClass("image-container");
+			imgTag = $("<img>").attr("src", "images/"+dish.image).addClass("grid-border").addClass("img-responsive").addClass("img-thumbnail");
+			label = $("<p>").addClass("center-label").addClass("grid-border").addClass("grey").html(dish.name);
+			var dishPrice = model.getDishPrice(dish.id)*model.getNumberOfGuests();
+			courseCost = $("<p>").html(dishPrice + " SEK");
+			imgContainer.append(imgTag);
+			imgContainer.append(label);
+			imgContainer.append(courseCost);
+			imgCol.append(imgContainer);
+			imgRow.append(imgCol);	
+		}
+
+		dinnerCol.append(imgRow);
+
+		// Populate the total cost column
+		var costCol = $("<div>").addClass("left-border col-md-1");
+		totalCost = $("<p>").html("Total: <br>" + totalCost + " SEK");
+		costCol.append(totalCost);
+
+		// Append the total cost column and the dinner images colum to the first row
+		firstRow.append(dinnerCol);
+		firstRow.append(costCol);
+
+		// Add the second row which contains the get full recipe button, 
+		// also populate the button column and append it to the row.
+		var secondRow = $("<div>").addClass("top-border row");
+		var buttonCol = $("<div>").addClass("col-md-12");
+		var printButton = $("<button>").addClass("print-button btn btn-primary").attr("id", "dinnerOverViewButton").html("Print Full Recipe");
+		buttonCol.append(printButton);
+		secondRow.append(buttonCol);
+
+		container.append(firstRow);
+		container.append(secondRow);
+
 	}
 }
--- js/view/printPrepView.js
+++ js/view/printPrepView.js
@@ -13,7 +13,7 @@ var PrintPrepView = function (container,model) {
 		row.append(imgCol);
 
 		//Append ingredients
-		var dishCol = $("<div>").addClass("col-md-4");
+		var dishCol = $("<div>").addClass("col-md-4").attr("id", "dishCol");
 		var dishName = $("<h3>").html(dish.name);
 		dishCol.append(dishName);
 
@@ -38,6 +38,8 @@ var PrintPrepView = function (container,model) {
 		container.append(row);		
 	}
 
+	model.addObserver(this);
+
 
 	this.makeHidden = function(){
 		container.fadeOut(0, function() {
@@ -49,5 +51,47 @@ var PrintPrepView = function (container,model) {
 		container.fadeIn(1000, function() {
 			//Animation complete
 		});
+	}
+
+	this.update = function(){
+		container.html("");
+		var dishes = model.getFullMenu();
+
+		for(key in dishes){
+			var dish = dishes[key];
+			var row = $("<div>").addClass("row");
+
+			//Append the image
+			var imgCol = $("<div>").addClass("col-md-2 col-md-offset-1");
+			var img = $("<img>").addClass("img-responsive print-img").attr("src", "images/" + dish.image);
+			imgCol.append(img);
+			row.append(imgCol);
+
+			//Append ingredients
+			var dishCol = $("<div>").addClass("col-md-4").attr("id", "dishCol");
+			var dishName = $("<h3>").html(dish.name);
+			dishCol.append(dishName);
+
+			var ingredients = dish.ingredients;
+			var guests = model.getNumberOfGuests();
+			for(key2 in ingredients){
+				var ingredient = ingredients[key2];
+				var dishIngredient = $("<p>").html((ingredient.quantity*guests) + " " + ingredient.unit + " " + ingredient.name);
+				dishCol.append(dishIngredient);
+			}
+			
+			row.append(dishCol);
+
+			//Append the preperations instructions
+			var prepInfo = $("<div>").addClass("col-md-4");
+			var prepHead = $("<h4>").addClass("print-prep").html("Preperations");
+			var prepInstructions = $("<p>").html(dish.description);
+			prepInfo.append(prepHead);
+			prepInfo.append(prepInstructions);
+			row.append(prepInfo);
+
+			container.append(row);		
+		}
+
 	}
 }
